0e138c3af7e2097c27322b9079e424f681af597b
GRAND <bengrand.pro@gmail.com>
2020-03-25 15:33:47 +0100

    add first

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello Pierre!'));
+console.log(chalk.red('Hello Mickael!'));
+console.log(chalk.yellow('Hello François!'));
+console.log(chalk.green('Hello Greg!'));
